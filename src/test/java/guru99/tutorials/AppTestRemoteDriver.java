3f20ba808e4127563e2e68cf1a0fe529d733a495
Sel Herring <Sel@MacBook-Pro-Macbook.local>
2017-04-05 23:11:34 +0400

    Добавление локального Chrome драйвера

--- src/test/java/guru99/tutorials/AppTestRemoteDriver.java
+++ src/test/java/guru99/tutorials/AppTestRemoteDriver.java
@@ -1,10 +1,13 @@
 package guru99.tutorials;
 
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,21 +19,25 @@ public class AppTestRemoteDriver {
 
     @Test
     public void simpleTest() {
-        WebDriver driver = null;
+        //WebDriver driver = null;
         try {
-            driver = new RemoteWebDriver(new URL("http://localhost:32771/wd/hub/"),
-                    DesiredCapabilities.chrome());
-        } catch (MalformedURLException e) {
+            //File file = new File("/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome");
+            System.setProperty("webdriver.chrome.driver", "/Applications/Google Chrome.app/Contents/MacOS/chromedriver"); //file.getAbsolutePath());
+            driver = new ChromeDriver();
+//            driver = new RemoteWebDriver(new URL("http://localhost:32771/wd/hub/"),
+//                    DesiredCapabilities.chrome()) {
+//            };
+        } catch (NotFoundException e) {
             e.printStackTrace();
         }
 
-        String eTitle = "Meet Guru9ыв9";
+        String eTitle = "Meet Guru99";
         String aTitle = "";
 
         // запустить firefox и перейти по адресу
         driver.get("http://www.guru99.com/");
         // разворачивает окно браузера
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
         // получить значение у тайтла страницы
         aTitle = driver.getTitle();
 
